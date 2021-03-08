8221874987864a6b8c4284ccb33bbb2b3f4e9a3c
josayarh <johakim.sayarh@gmail.com>
2021-03-08 17:08:45 +0100

    push site

---
+++ src/yourdata.js
@@ -0,0 +1,210 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+import unityIcon from "./images/unity.svg"
+import cppIcon from "./images/cpp.svg"
+import csharpIcon from "./images/c-sharp.svg"
+import gitHubIcon from "./images/github.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Jo-Hakim Sayarh",
+  headerTagline: [
+    //Line 1 For Header
+    "Junior Developer",
+    //Line 2 For Header
+    "with a passion for games",
+    //Line 3 For Header
+    "and boundless curiosity",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Hi I am a French Junior developer with experience in video game development and R&D.",
+
+  //Contact Email
+  contactEmail: "johakim.sayarh@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "A replay framework for Unity", //Project Title - Add Your Project Title Here
+      para:
+        "A simple framework for a replay system in Unity", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://cdn.discordapp.com/attachments/761596669925654584/818443886702755860/Replay.PNG",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/josayarh/UnityReplaySystem",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Raiders of the Lost Shark", //Project Title - Add Your Project Title Here
+      para:
+        "A platform game for the Global Game jam 2021, made with another developer, 2 artists, 1 composer and 1 game designer. It is a 2d platformer with some " +
+        "Metroidvania influences.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://ggj.s3.amazonaws.com/styles/game_sidebar__wide/featured_image/2021/01/240901/ecran_titre_avec_shark.jpg?itok=5iVBLjOt&timestamp=1612120743",
+      //Project URL - Add Your Project Url Here
+      url: "https://drive.google.com/drive/folders/1P8SDBcchKHNyc7leY71PZVs83r3z0zFj?usp=sharing",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Curse of the night", //Project Title - Add Your Project Title Here
+      para:
+        "Student project made with Unity over 4 months and organized using Scrum with a team of 6 people. The only 3D game in this list, we wanted to make an action infiltration " +
+        "game.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://media.discordapp.net/attachments/761596669925654584/818503715447242763/unknown.png?width=1243&height=683",
+      //Project URL - Add Your Project Url Here
+      url: "https://drive.google.com/drive/folders/1jnyQ3KGdzJC9Y688uHj82pMAIdcJy536?usp=sharing",
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Huile de foie de morue i.e. Marée noire Simulator", //Project Title - Add Your Project Title Here
+      para:
+        "A small game jam game developed in a little over 24 hours with another developer and another person providing sprites and dialogue. It's an infinite runner with a " +
+        "very humoristic tone and simple gameplay. It is my first game jam game.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://cdn.discordapp.com/attachments/761596669925654584/818451240329543680/unknown.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://drive.google.com/file/d/1qQAjfEERvgCqaolVSkTarTgyKkka9xHB/view?usp=sharing",
+    },
+    {
+      id: 5, //DO NOT CHANGE THIS (Please)😅
+      title: "Pokeman", //Project Title - Add Your Project Title Here
+      para:
+        "A game jam game developed over 36 hours with 2 other developers, 1 artist and 1 composer. It's a pokemon parody where animals tame humans and make " +
+        "them fight in a turn base rpg.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://media.discordapp.net/attachments/677802706945048587/678476165366677563/Ecran_titre.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://drive.google.com/file/d/1-LMbE2S-0rCRefDJUdUVCYbzYKdE1vcO/view?usp=sharing",
+    },
+    {
+      id: 6, //DO NOT CHANGE THIS (Please)😅
+      title: "Global Game Jam 2019", //Project Title - Add Your Project Title Here
+      para:
+        "A puzzle-platformer game developed with 2 other developers, 3 artists and 1 composer. ", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://media.discordapp.net/attachments/761596669925654584/818480517661589564/unknown.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://drive.google.com/file/d/1Yi4isnM63i8Xwt7BS2qk82N9mDXyujFD/view?usp=sharing",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+    ,{
+        id: 5,
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Hi I'm a software engineering graduate with experience in game development and R&D. I've studied at the Institut Supérieur d'Informatique, de Modélisation et de leurs Applications" +
+    " in Clermont Ferrand with several stays abroad, 1 year in Canada, 5 months in Ireland, 2 months in Brazil, either as part of my studies or for internships.",
+  aboutParaTwo:
+    "During my studies I have specialized myself in Video game development, mostly with the Unity engine but I've also used Unreal and Godot to a lesser degree. " +
+    "Development in Unity means I've become quite proficient with C# but I keep a particular fondness for C++ and I try to keep myself up to date with the updates to the " +
+    "language even though I don't practice it as often as I used too.",
+  aboutParaThree:
+    "I am off course a Video game fan, mostly rpgs and action games nowadays, and I'm very active in some gaming communities. I am an admin and moderator on several " +
+    "large discord servers the largest counting 1000 members and I also help moderate several twitch and youtube channels with livestreams that can range between 50 to 100 consecutive " +
+    "viewers.",
+  aboutImage:
+    "https://media.discordapp.net/attachments/761596669925654584/818487833161498674/324767_274289422684437_1629327097_o.jpg",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      id: 1,
+      img: unityIcon,
+      para:
+        "I've been regularly working with Unity for the past 4 years and getting better at it all the time. All the projects above where developed with this game engine.",
+    },
+    {
+      id: 2,
+      img: csharpIcon,
+      para:
+        "I've been using C# mostly for unity scripting but I am also familiar with linq and the ASP.Net framework.",
+    },
+    {
+      id: 3,
+      img: cppIcon,
+      para:
+        "I have worked extensively with C++ throughout my studies and I try to keep myself up to date on the language with most notably the cppcon conferences.",
+    },
+    {
+      id: 4,
+      img: gitHubIcon,
+      para:
+        "I have used git hub during most of my studies and am familiar with using it as well as other versioning software like tortoise SVN",
+    },
+    {
+      id: 5,
+      img: designIcon,
+      para:
+        "Thanks to the experience i have developing games either solo or with small teams i have familiarized myself with game design theory and 2d art techniques",
+    },
+    {
+      id: 6,
+      img: codeIcon,
+      para:
+        "I have also worked with other development languages like Java, Javascript, Bash, Html, Php, Css, as well as frameworks and IDEs like the jet brain IDEs Clion, Rider, Android studio " +
+        "Visual studio, React ...",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  // promotionHeading: "Heading",
+  // promotionPara:
+  //   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/josayarh" },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
